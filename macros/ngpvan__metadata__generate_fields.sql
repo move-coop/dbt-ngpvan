a310019b7215d0c92e043a3f475bc3096f416b0b
anneram <anne@withdatalove.com>
2023-11-07 16:44:41 -0500

    fix metadata fields macro

--- macros/ngpvan__metadata__generate_fields.sql
+++ macros/ngpvan__metadata__generate_fields.sql
@@ -4,16 +4,16 @@
 
 
 {% macro default__ngpvan__metadata__generate_fields(segment_by_column, myvoters) -%}
-    SPLIT(REPLACE(_dbt_source_relation, '"', ''), '.')[1] AS source_schema,
-    SPLIT(REPLACE(_dbt_source_relation, '"', ''), '.')[2] AS source_table
+    SPLIT(REPLACE(_dbt_source_relation, '`', ''), '.')[1] AS source_schema,
+    SPLIT(REPLACE(_dbt_source_relation, '`', ''), '.')[2] AS source_table
 
     {%- if myvoters == true -%}
     ,
-    CASE WHEN RIGHT(SPLIT(REPLACE(_dbt_source_relation, '"', ''), '.')[2], 3) IN ('_vf', 'myv')
+    CASE WHEN RIGHT(SPLIT(REPLACE(_dbt_source_relation, '`', ''), '.')[2], 3) IN ('_vf', 'myv')
             THEN 0
             ELSE 1
         END AS database_mode,
-    CASE WHEN RIGHT(SPLIT(REPLACE(_dbt_source_relation, '"', ''), '.')[2], 3) IN ('_vf', 'myv')
+    CASE WHEN RIGHT(SPLIT(REPLACE(_dbt_source_relation, '`', ''), '.')[2], 3) IN ('_vf', 'myv')
             THEN TRUE
             ELSE FALSE
         END AS is_myvoters
