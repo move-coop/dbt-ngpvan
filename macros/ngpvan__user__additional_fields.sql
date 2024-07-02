8a7bf8dba1bb98bebb2d1c30bc1d32d0ea5e32b7
Ian <47256454+IanRFerguson@users.noreply.github.com>
2024-07-02 11:11:14 -0400

    beta additional fields

---
+++ macros/ngpvan__user__additional_fields.sql
@@ -0,0 +1,24 @@
+-- This macro provides support for additional fields that the user wishes
+-- to pull through the `dbt-ngpvan` pipeline from their own workflow (for instance,
+-- additional fields provided by a vendor)
+
+{%- macro ngpvan__user__additional_fields(base_ref) -%}
+    {{ return(adapter.dispatch('ngpvan__user__additional_fields', 'dbt_ngpvan')(base_ref=base_ref)) }}
+{%- endmacro %}
+
+{%- macro default__ngpvan__user__additional_fields(base_ref) -%}
+    {%- if config("additional_fields") -%}
+        {%- set columns = dbt_utils.get_filtered_columns_in_relation(from=ref(base_ref)) -%}
+        {%- for field in config("additional_fields") -%}
+            
+            {%- if field in columns -%}
+                ,
+                {{ field }}
+            {%- else -%}
+                NULL AS {{ field }}
+            {%- endif -%}
+
+        {%- endfor -%}
+    {%- endif -%}
+
+{%- endmacro %}
