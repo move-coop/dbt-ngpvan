8e325c9971271fff2c8f71a8f631c20fbcf937ca
Ian <47256454+IanRFerguson@users.noreply.github.com>
2025-01-23 14:56:24 -0500

    clean up incremental where

--- models/staging/ngpvan/core/stg_ngpvan__contacts_contacts.sql
+++ models/staging/ngpvan/core/stg_ngpvan__contacts_contacts.sql
@@ -1,9 +1,3 @@
-{%- set partitions_to_replace = generate_partitions_to_replace(
-        incremental_window=var('dbt_ngpvan_config')["ngpvan_default_incremental_window_days"],
-        date_part="day"
-    ) 
--%}
-
 {{
     config(
         materialized="incremental",
@@ -27,7 +21,7 @@ WITH
         
         FROM {{ ref('base_ngpvan__contactscontacts') }}
         {% if is_incremental() %}
-        WHERE TIMESTAMP_TRUNC(datecanvassed, DAY) IN ({{ partitions_to_replace | join(",") }})
+        WHERE DATE_DIFF(CURRENT_DATE, datecanvassed, DAY) <= {{ var("ngpvan_default_incremental_window_days") }}
         {% endif %}
     ),
 
