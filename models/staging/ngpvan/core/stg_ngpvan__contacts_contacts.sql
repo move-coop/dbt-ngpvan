1fe8e7ce5f3f4ed30a0b9556d2d86b14facd2959
Ian <47256454+IanRFerguson@users.noreply.github.com>
2025-01-23 15:15:41 -0500

    add comment

--- models/staging/ngpvan/core/stg_ngpvan__contacts_contacts.sql
+++ models/staging/ngpvan/core/stg_ngpvan__contacts_contacts.sql
@@ -1,3 +1,4 @@
+-- NOTE - This is a TMC macro, see `dbt-tmc`
 {%- set partitions_to_replace = generate_partitions_to_replace(
         incremental_window=var('dbt_ngpvan_config')["ngpvan_default_incremental_window_days"],
         date_part="day"
