3fa1585a925c2ac26ac8d14f82de3da921fa5f59
Willy Raedy <wraedy@gmail.com>
2025-03-04 13:36:57 -0600

    fix date formatting on normalize utc macro

--- macros/normalize_timestamp_to_utc.sql
+++ macros/normalize_timestamp_to_utc.sql
@@ -3,9 +3,9 @@
     CASE WHEN {{ time_stamp }} IS NOT NULL AND TRIM(CAST({{ time_stamp }} AS STRING)) != ''
 
     {% if convert_from_timezone != 'UTC' %}
-            THEN FORMAT_TIMESTAMP('%G-%m-%d %H:%M:%S', TIMESTAMP(CAST({{ time_stamp }} AS STRING), '{{ convert_from_timezone }}'), '{{ convert_to_timezone }}' )
+            THEN FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:%S', TIMESTAMP(CAST({{ time_stamp }} AS STRING), '{{ convert_from_timezone }}'), '{{ convert_to_timezone }}' )
     {% else %}
-            THEN CAST(FORMAT_TIMESTAMP('%G-%m-%d %H:%M:%S', TIMESTAMP( {{ time_stamp }}), '{{ convert_to_timezone }}' )  AS TIMESTAMP)
+            THEN CAST(FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:%S', TIMESTAMP( {{ time_stamp }}), '{{ convert_to_timezone }}' )  AS TIMESTAMP)
     {% endif %}
             ELSE NULL
         END
