945ea7d1e84129b4f9463dbb0fa407db170acf21
Kasia Hinkson <52927664+KasiaHinkson@users.noreply.github.com>
2025-03-06 15:40:13 -0600

    distinct is enough

--- models/staging/ngpvan/core/stg_ngpvan__contacts_contacts.sql
+++ models/staging/ngpvan/core/stg_ngpvan__contacts_contacts.sql
@@ -89,4 +89,3 @@ WITH
     )
 
 SELECT DISTINCT * FROM renamed
--- QUALIFY ROW_NUMBER() OVER (PARTITION BY vendor_unique_stg_ngpvan__contacts_contacts_id ORDER BY utc_modified_at DESC NULLS LAST) = 1
