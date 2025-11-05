399966eaff9c327ea7ffe4f62ef29bf5074f90d3
hannah-marcus28 <hannah@movementcooperative.org>
2025-11-05 12:42:29 -0500

    adding more join keys to intermediate models

--- models/intermediate/ngpvan/int_ngpvan__01__admins.sql
+++ models/intermediate/ngpvan/int_ngpvan__01__admins.sql
@@ -95,7 +95,9 @@ WITH
             {{- ngpvan__int__additional_fields() }}
 
         FROM all_users users
-        LEFT JOIN user_groups USING (user_id) 
+        LEFT JOIN user_groups 
+            ON users.user_id = user_groups.user_id 
+                AND users.segment_by = user_groups.segment_by
         GROUP BY           
             users.user_id,
             users.username,
